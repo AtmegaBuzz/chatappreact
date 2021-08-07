59a9d48c03efef90103e704a612e77ef48b87883
swapnil <68425016+AtmegaBuzz@users.noreply.github.com>
2021-08-07 12:36:18 +0530

    removed my data

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,8 @@
 import firebase from "firebase/app";
 import "firebase/database"; 
 
-var firebaseConfig = {
-  apiKey: "AIzaSyDgl6XkQ-jevm7hfEY-flGkxXLydvYW5t0",
-  authDomain: "wattsapp-swap.firebaseapp.com",
-  databaseURL: "https://wattsapp-swap-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "wattsapp-swap",
-  storageBucket: "wattsapp-swap.appspot.com",
-  messagingSenderId: "281196754922",
-  appId: "1:281196754922:web:86a457db25d35c090a2493"
-};
+var firebaseConfig = add your firebase config by creating firebase account and then add app then copy the config given to this file
+
 // Initialize Firebase
 const firebasedb = firebase.initializeApp(firebaseConfig);
 
